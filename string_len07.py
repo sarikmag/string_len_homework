4b0402aba1b69f9f1c994bfab181287b825d83ee
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-10-31 09:44:47 +0500

    adss

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,17 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    s=""
+    s+="["
+    if len(s1)%2==1:
+        s+=s1
+    if len(s2)%2==1:
+        if len(s)>1:
+            s+=", "
+        s+=s2
+    if len(s3)%2==1:
+        if len(s)>1:
+            s+=", "
+        s+=s3
+    return s+"]"
+print(main("code", "cde", "reqwe"))
